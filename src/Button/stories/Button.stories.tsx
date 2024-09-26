1c9beb86f0fd530396e034f186360636201bca28
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-09-26 20:49:03 +0300

    initial commit

---
+++ src/Button/stories/Button.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Meta, StoryFn } from '@storybook/react';
+import { Button, ButtonProps } from '../ui/Button';
+
+const meta: Meta = {
+  title: 'Welcome',
+  component: Button,
+  argTypes: {
+    children: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+  parameters: {
+    controls: { expanded: true },
+  },
+};
+
+export default meta;
+
+const Template: StoryFn<ButtonProps> = (
+  args: React.JSX.IntrinsicAttributes & ButtonProps
+) => <Button {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  onClick: () => alert('hello'),
+  children: 'test button',
+};
