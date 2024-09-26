9d8ee38fd7f3f64d2adab539096efb025ec23d14
SinitsaBogdan <124495463+SinitsaBogdan@users.noreply.github.com>
2024-09-26 19:28:56 +0000

    Deploying to gh-pages from @ SinitsaBogdan/abstractum@360d0de4b28aa2b4eeb1686c38e301953e348042 🚀

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.[t|j]sx?$': 'babel-jest',
-  },
-  moduleNameMapper: {
-    '^.+\\.(css|less|scss)$': 'babel-jest',
-  },
-};
